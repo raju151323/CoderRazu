a33d344bc1223576f325258d7fe91b4b6a6c2e26
raju151323 <73578932+raju151323@users.noreply.github.com>
2021-01-02 14:20:01 +0600

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,529 @@
+alert("hello") //for alert fountion
+
+typeof() //for a data type check 
+
+//Create comment // for a single line comment; /* for a mulitiple line comment
+
+
+
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//////***************************varriable***************/////////
+//dOWN
+
+
+/////****ভেরিয়েবল হচ্ছে, যেখানে সব গুলা ডাটাকে ডুকিয়ে বা ভরে রাখা হয়।বার বার যেনো না লিখতে হয়। যেমনঃ-
+var name: 'pervez alam' 
+var age: 22
+এখানে name টা হচ্ছে ভেরিয়েবল। এই ভেরিয়েবলের মধ্যে pervez alaam রাখা হয়ছে।*****।\\\\\\\\
+
+যে  ওয়ার্ডগুলো ভেরিয়েবল নামে ইউস করতে পারবনা---
+
+abstract	arguments	await*	boolean
+break	byte	case	catch
+char	class*	const	continue
+debugger	default	delete	do
+double	else	enum*	eval
+export*	extends*	false	final
+finally	float	for	function
+goto	if	implements	import*
+in	instanceof	int	interface
+let*	long	native	new
+null	package	private	protected
+public	return	short	static
+super*	switch	synchronized	this
+throw	throws	transient	true
+try	typeof	var	void
+volatile	while	with	yield########
+
+
+
+
+
+
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+/*****Data type*******************************/////
+
+primitive data:-
+number:- 1,2,3,4,- 1.5-2.5-7.8 intiger number= 45.. float number=45.6
+String:- "raju" or 'raju" এইখানে কোটেশনের মধ্যে যা থাকবে তা ই হচ্ছে string/
+Boolean:- true and false..
+Undefined
+
+/*Number*/
+var n = 45
+var a = 34
+var nn = Number("45") এইভাবে string এ ও নাম্বার রাখতে পারি/
+console.log(Number.parsefloat(nn)) for print. + convert float/ এইভাবে নাম্বার থেকে দশমিকে কনভার্ট
+ করতে পারি/
+দশমিক নাম্বারকে float বলে। আর ১২৩ int নাম্বার বলে।
+
+
+Constrac যেভাবে করে। var n = Numeber(32)
+
+
+
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+//*********************String to Number and Number to string********** */
+
+var n = '34'
+var n2 = 23
+
+console.log(Number.parseInt(n))
+console.log(n2.toString())
+
+ /// Concatenatoin or concatenated***/// pending
+//EXAMPLE
+var raju = "pervez"
+var raju2 = "juwel"
+var raju3 = "jihan"
+var raj = "pervez" + 
+console.log()
+
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//*******************OPERATOR***************** *///
+//DOWN
+
+arithmatic operator
+(+,-,/,*,% ++ --)
+
+% modules ভাগশেষ বের করার জন্য/
+++ incrimale
+
+//for pre incrimale
+var a = 11
+var b =4
+console.log(++a) এইভাবে দিলে ১ বাড়বে।
+
+//for post incrimale
+var a = 11
+var b =4
+console.log(a++) এইভাবে দিলে ১ বাড়বে।বাট ২বার প্রিন্ট করতে হবে/
+
+-- এর ক্ষেত্রেও সেম//  
+
+// -=, +=, /=, %= operators//
+
+var a = 30
+var b = 20
+
+a +=b এর মানে a= a=a+b, সব গুলা সেম/
+
+//* comparison Operator***///
+comparison operator সব সময় bolean result দে, যেমন true or false/
+
+// == != এর কাজ//
+var a = 10
+var b = 20 
+console.log( a == b) //false আসবে কারন ২টার ডাটা সেম না।।
+console.log( a != b) //true এইখানে সমান না কিনা চেক করচে, নেগেটিব চেক করে/
+
+
+// >, <, >=, <= এর কাজ//
+
+var a = 20
+var b = 30
+console.log(a > b) //false এই চেক করা হয়ছে a , b  এর থেকে বড নাকি ছোট/
+console.log(a < b) //true এই চেক করা হয়ছে b , a  এর থেকে বড নাকি ছোট/
+console.log(a >= b) //false এইখানে বড বা সমান কিনা চেক করচে/
+console.log(a >= b) //false এইখানে বড বা সমান কিনা চেক করচে/
+
+
+// ==, ===, for data type check/
+
+var a = "20"
+var b = 30
+console.log(a == b) //true nd data type r value চেক করে নাই।
+console.log(a === b) //false nd data type or value চেক করছে/
+
+
+//Other operator/
+&& = and operator, এইটা হয় একটা কন্ডিশন সত্য হবে, নাহয় অইতা সত্য হবে,
+।। = or operator, এইটা ২তাই কন্দিশন সত্য হবে/
+! = not operator, 
+
+binary number er jonno./
+& ,|,
+
+//data type ck for//
+console.log(typeof "string")
+
+
+
+
+
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+// *************************************Fountion************//////
+//DOWN//
+
+
+//*************math fountions************** */
+//*************math fountions************** */
+//*************math fountions************** */
+//*************math fountions************** */
+//*************math fountions************** */
+//*************math fountions************** */
+
+// FOR E VALUE//
+console.log(Math.E)
+// FOR PI VALUE//
+console.log(Math.PI)
+// FOR FLOOR VALUE//
+var a = 4.589
+console.log(Math.floor(n)) মানে দশমিকের আগে সংখা হবে/
+
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.ceil(n))
+console.log(Math.max(400,500,600)) সব চেয়ে বড নাম্বার বের করতে,
+console.log(Math.min(400,500,600)) সব চেয়ে ছোট নাম্বার বের করতে,
+console.log(Math.pow(2,3)) পাওয়ার বের করার জন্য।
+console.log(Math.sqrt(2,3)) স্ক্যায়ারের জন্য/
+console.log(Math.round(Math.random() * 50 + 1)) ১ থেকে ৫০ এর মধ্য রেন্ডম নাম্বার বের করার জন্য/
+
+
+
+//*************Date fountions************** */
+
+//*************Date fountions************** */
+
+//*************Date fountions************** */
+
+//*************Date fountions************** */
+
+//*************Date fountions************** */
+
+//*************Date fountions************** */
+
+বর্তমান সময়, তারিখ, বছর জানার নিয়ম,
+var date = new date()
+
+console.log(date)
+console.log(date.toDateString()) সব string e পাওয়ারজন্য/
+console.log(date.toTimeString()) 
+console.log(date.toLocalString()) 
+
+console.log(date.getFullYear()) 
+console.log(date.getFullMonth()) 
+console.log(date.getFullDate()) 
+console.log(date.getFullHour()) 
+console.log(date.getFullMinute()) 
+console.log(date.getFullSecond()) 
+
+
+
+
+
+
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+//********CONDITION***************** */
+
+
+
+//****If and else Conditon*** */
+
+
+var a = 30
+var b = 40
+
+if (condition){
+
+
+console.log(Text)
+
+
+}
+
+var a = 20
+var b = 30
+
+if(a > b){
+
+
+console.log("its false")
+
+}
+
+else{
+
+    console.log("Its true")
+}
+
+//**********Else if********/
+
+
+var a = 20
+var b = 20
+
+if(a > b){
+
+console.log("its true")
+
+}else if(a < b){
+
+    console.log("its true")
+    
+    }}else {
+
+        console.log("They are same")
+        
+        }
+
+
+
+
+        //*******While statement********* */
+
+     without Bolean value-
+
+     var date = new Date()
+
+     //0=sunday, 1=monday, 2=tuesday,...........
+
+     var today =date.getDay()
+
+     switch(today){
+
+        case:0
+        console.log("Today is Sunday")
+
+        case:1
+        console.log("Today is Monday")
+
+        case:2
+        console.log("Today is Tuesday")
+
+        case:3
+        console.log("Today is Wednessday")
+
+        case:4
+        console.log("Today is Thursday")
+
+        case:5
+        console.log("Today is Friday")
+
+        case:6
+        console.log("Today is Saturday")
+
+
+
+     } এইখানে যেটা সঠিক দিন সেটা বের হবে/ এইখানে আজকের দিনটা বের করা হয়ছে/
+
+
+
+     //*******Logical Operator******************* */
+      //*******Logical Operator******************* */
+       //*******Logical Operator******************* */
+        //*******Logical Operator******************* */
+         //*******Logical Operator******************* */
+          //*******Logical Operator******************* */
+           //*******Logical Operator******************* */
+            //*******Logical Operator******************* */
+             //*******Logical Operator******************* */
+              //*******Logical Operator******************* */
+
+
+
+
+&& and operator, || Or operator, ! NOt operator
+
+
+A && B
+দুইটাই  যদি true হয়, তাহলে true হবে, আর বাকি  সব false/
+
+A || B
+দুইটার যদি false হয়,তাহলে false হবে, আর বাকি সব true/
+
+
+var  a = 20
+var b = 30
+var c = 40
+var d = 50
+
+if(a < b && c < d){
+
+
+console.log("its ture Value")
+}
+else(a > b && c < d){
+
+    console.log("its false value")
+}
+
+
+
+if(a > b || c > d){
+
+
+console.log("Its False value")
+}
+
+if(a < b || c < d){
+
+
+    console.log("Its true value")
+    }
+
+
+
+
+
+//*******Ternary Operator/-Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+//*******Ternary Operator/Condition এর বিকল্প সরটকাট******/
+
+var a = 10
+var b = 20
+var result = a<b ? "its right" : "its Wrong"
+
+// var = condition ? "true" : "false"
+
+
+//******And, or Use Shortcut/For a Condition******/
+//******And, or Use Shortcut/For a Condition******/
+//******And, or Use Shortcut/For a Condition******/
+
+
+var name = '' //Empty String always false value
+var fullName =name || 'Mr raju'
+console.log(fullName)
+
+
+
+
+//******************************"For" Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************/
+//******************************For Loop***************************///******************************For Loop***************************/
+
+//******************************For Loop***************************/
+
+
+for (var i = 0; i < 100; i++){
+
+console.log(i)
+
+}
+
+// for (var i=যতথেকে শুরু হবে; i < কত পর্যন্ত চলবে; সাথে কত যোগ হবে,
+//i++ দিলে ১ যোগ হবে, আর i+=1 এইভাবে দিলে ২ যোগ হবে){
+    console.log(i + "raju") যেটা প্রিন্টকরতে চাই
+}
+
+
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+//**********************"While" Loop*********************************8*/
+
+
+
+var isRunning = true
+
+while(isRunning){
+    var rand =((Math.floor)Math.random() * 10 + 1)//create random number 1-10 er vitor
+    if(rand === 9){
+
+        console.log('winner winner chicken dinner')
+        isRunning=false //winner পাওয়ার পর loop অফ হওয়ার জন্য/
+    } else{
+
+        console.log('you are got' + rand)
+    }
+
+}
+
+while loop এ যখন  var = false থাকে তখন do এর কাজ আসে, যেমন---
+
+var isRunning = false
+
+while(isRunning){
+
+    console.log(' I am running')
+}
+do {
+
+    console.log('I am running')
+} while(isRunning)
